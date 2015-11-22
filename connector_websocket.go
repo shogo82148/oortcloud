2b53f5461a1a3f9f2bc9459c1c7b29c11aff1264
Ichinose Shogo <shogo82148@gmail.com>
2015-11-22 19:12:01 +0900

    send data from the server to the client

--- connector_websocket.go
+++ connector_websocket.go
@@ -17,6 +17,7 @@ type WebSocketConnection struct {
 	connector *WebSocketConnector
 	id        string
 	ws        *websocket.Conn
+	ch        chan []byte
 }
 
 func NewWebSocketConnector(notifier Notifier, binary bool) *WebSocketConnector {
@@ -49,7 +50,14 @@ func NewWebSocketConnector(notifier Notifier, binary bool) *WebSocketConnector {
 
 // Handle implements the http.Handler interface
 func (c *WebSocketConnector) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	id, resp, err := c.notifier.Connect(nil, req)
+	ch := make(chan []byte, 16)
+	defer close(ch)
+	conn := &WebSocketConnection{
+		connector: c,
+		ch:        ch,
+	}
+
+	id, resp, err := c.notifier.Connect(conn, req)
 	if err != nil || resp == nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -73,22 +81,30 @@ func (c *WebSocketConnector) ServeHTTP(w http.ResponseWriter, req *http.Request)
 	resp.Body.Close()
 	defer c.notifier.Disconnect(id)
 
-	conn := &WebSocketConnection{
-		connector: c,
-		id:        id,
-	}
+	conn.id = id
 	websocket.Handler(conn.serveWebsocket).ServeHTTP(w, req)
 }
 
 func (c *WebSocketConnection) serveWebsocket(ws *websocket.Conn) {
 	c.ws = ws
+	// send loop
+	go func() {
+		for sendData := range c.ch {
+			c.connector.codec.Send(ws, sendData)
+		}
+	}()
 
 	// receive loop
-	var data []byte
+	var receiveData []byte
 	for {
-		if err := c.connector.codec.Receive(ws, &data); err != nil {
+		if err := c.connector.codec.Receive(ws, &receiveData); err != nil {
 			return
 		}
-		c.connector.notifier.Notify(c.id, data)
+		c.connector.notifier.Notify(c.id, receiveData)
 	}
+}
+
+func (c *WebSocketConnection) Send(data []byte) error {
+	c.ch <- data
+	return nil
 }
